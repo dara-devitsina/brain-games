e8f9115d9177b7b1a97000204202d52c2f588c64
dara <dashadevitsina@gmail.com>
2020-05-31 08:34:52 +0300

    updated logic for "brain-even" game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { brainEven } from '../src/even.js';
+import brainEven from '../src/even.js';
 
 console.log(brainEven());
--- src/even.js
+++ src/even.js
@@ -1,42 +1,42 @@
 import readlineSync from 'readline-sync';
 
-export const brainEven = () => {
-
+const brainEven = () => {
 
 let numOfQuestions = 0;
 
-while (numOfQuestions <= 3) {
-    const randomNum = Math.floor(Math.random() * 1000000);
-    const query = `Question: ${randomNum}`;
-    console.log(query);
-      let usersAnswer = readlineSync.question();
+    while (numOfQuestions < 3) {
+        const randomNum = Math.floor(Math.random() * 1000000);
+        const query = `Question: ${randomNum}`;
+        console.log(query);
+        let usersAnswer = readlineSync.question();
 
-    if (randomNum % 2 === 0) {
-  
-        if (usersAnswer === 'yes') {
-            return 'Correct!';
-            numOfQuestions += 1;
-        }
-            else {
-                return `"no" is wrong answer ;(. Correct answer was "yes". Let's try again, !`
-            }
-     }
+        if (randomNum % 2 === 0) {
 
-    else if (randomNum % 2 !== 0) {
-        if (usersAnswer === 'no') {
-            return 'Correct!';
-            NumOfQuestions += 1;
-        }
-            else {
-                return `"yes" is wrong answer ;(. Correct answer was "no". Let's try again, !`
+            if (usersAnswer === 'yes') {
+                console.log('Correct!');
+                numOfQuestions += 1;
             }
+            else {
+                console.log(`"no" is wrong answer ;(. Correct answer was "yes". Let's try again, !`)
+                break;
+            }
+        }
+
+        else {
+            if (usersAnswer === 'no') {
+                console.log('Correct!');
+                numOfQuestions += 1;
+            }
+
+            else {
+                console.log(`"yes" is wrong answer ;(. Correct answer was "no". Let's try again, !`)
+                break;
+            }
+        }
     }
-
-}
-
-       
 if (numOfQuestions === 3) {
-    return `Congratulations, !`
+    return 'Congratulations, !';
 }
 };
 
+export default brainEven;
